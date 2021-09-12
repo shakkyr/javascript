3a6b44cb3c4099e0622ac2f8e70db488bf126343
Shadi Rada <shakkyr@gmail.com>
2021-09-12 18:12:38 +0300

    task 2.2

---
+++ ex 2-2/ex2-2.js
@@ -0,0 +1,6 @@
+function tellFortune (a, b , c ,d) {
+    return (`you will be a ${a} in ${b} and married to ${c} with ${d} children`);
+}
+
+shad = tellFortune("programmer", "haifa", 'mays', 2);
+// console.log(shad);
