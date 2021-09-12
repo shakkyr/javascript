71f59028c94ac89ff11c0644bbd244c2f695d752
Shadi Rada <shakkyr@gmail.com>
2021-09-12 15:52:54 +0300

    task 1.2

---
+++ ex 1-2/ex 1-2.js
@@ -0,0 +1,75 @@
+
+--Delete the wrong answers--
+
+1. Which of the following is/are strings?
+
+
+c) '4'
+
+2. Which of the following is/are numbers?
+
+a) 4
+b) 4.0
+
+d) -4
+
+3. Which of the following is/are booleans?
+
+a) true
+b) false
+
+
+4. What is the result of 80 + 71.2?
+
+a) 151.2
+
+
+5. What is the result of "80" + 71.2?
+
+
+c) "8071.2"
+
+6. Does 100 + 30 produce a number or a string?
+
+a) number
+
+
+7. Does "100" + 30 produce a number or a string?
+
+
+b)string
+
+*/
+
+//------Submit your answers under the question------//
+
+//create three different ways to declare variables
+
+we can use "let"  "var"  "const"
+
+//declare a variable and reassign a value to it
+
+let sha1 =4;
+sha1=100;
+
+//create a variable and after that assign a string with its value being: He's got it!
+cosnt sha2 = "he's got it";
+/*
+1. create a variable and assign a value on how much a restaurant bill is.*/
+
+let bil1 = "the resturant bill is";
+let val1 = 450;
+
+console.log(bil1, val1);
+// 2. create a variable and assign a value on how much tax they should pay.
+const tax1= val1 * 0.17;
+// 3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
+ let out1 =`Your total bill is ${val1 + tax1 }$`
+
+// Round the number 50.6 to its nearest integer//
+Math.round(50.6);
+
+
+//Create a variable that is undefined
+
+let rand1;
--- ex 1.1/ex1-1.js
+++ ex 1.1/ex1-1.js
@@ -5,12 +5,3 @@ let bol1 = (10>9)  // 4. Create a variable that its value holds a boolean.
 let nl = null ; //5. Create a variable that its value holds null.
 let und1  // 5. Create a variable that its value is undefined.
 let sym1 = Symbol('*g&')
-
-
-console.log('str1 string value :', str1);
-console.log('num1 variable of number :', num1);
-console.log("obj1 valiable of object :", obj1);
-console.log("bol1 boolean :", bol1);
-console.log("nl null value: ", nl);
-console.log("und1 undifined variable :", und1);
-console.log("sym1 variable contains a symbol:", sym1);
