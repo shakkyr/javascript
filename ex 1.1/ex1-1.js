f87aa27068303c945c950c7baf43a3ca28b01f98
Shadi Rada <shakkyr@gmail.com>
2021-09-13 07:45:30 +0300

    2.4

--- ex 1.1/ex1-1.js
+++ ex 1.1/ex1-1.js
@@ -1,7 +1,16 @@
-let str1 = "hello world"  //1. Create a variable that’s value holds a string.
-let num1 = 5  //2. Create a variable that its value holds a number.
+let str1 = "hello world";  //1. Create a variable that’s value holds a string.
+let numm = 500;  //2. Create a variable that its value holds a number.
 const obj1 = {firstname :'shadi', lastname :'rada', age :'34'}  //3. Create a variable that its value holds an object.
 let bol1 = (10>9)  // 4. Create a variable that its value holds a boolean.
-let nl = null ; //5. Create a variable that its value holds null.
+let age=null; //5. Create a variable that its value holds null.
 let und1  // 5. Create a variable that its value is undefined.
 let sym1 = Symbol('*g&')
+
+
+console.log(typeof(str1));
+console.log(typeof(numm));
+console.log(typeof(obj1));
+console.log(typeof(bol1));
+console.log(typeof(age));
+console.log(typeof(und1));
+console.log(typeof(sym1));
