2caf046215c315bb4ad816a1bb3eb4af84e185d9
Shadi Rada <shakkyr@gmail.com>
2021-09-13 20:20:05 +0300

    4-4

--- ex 4-4/ex4-4.js
+++ ex 4-4/ex4-4.js
@@ -1,13 +1,20 @@
+let number = 7;
+number = number.toString();
+
+
 function printNumbers(n){
+
     
-    for (i=0 ; i<=n ;i++) {
-        if (i.includes(7)){
-            console.log("boom");
+    for (let i=0 ; i<=n ;i++) {
+        if (i%7 ===0 && i.toString().includes('7')) {
+            console.log("boom,boom", i);
+        }
+       
+        else if (i % 7 === 0 ){
+            console.log("boom", i);
         }
 
-        // else if ((i%7 ==0) || (i.includes(7))) {
-        //     console.log("boom,boom");
-        // }
+            
         else 
         {
         console.log(i);
