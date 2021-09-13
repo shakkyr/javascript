bed316bb5a24f42460da9c82eed152996fdbe1c8
Shadi Rada <shakkyr@gmail.com>
2021-09-13 09:10:37 +0300

    2.4

--- ex 2-4/ex2-4.js
+++ ex 2-4/ex2-4.js
@@ -3,28 +3,21 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
 ];
-​
 function getDayName() {
     return days[date.getDay()]
 }
-​
 function getDayOfMonth() {
     return date.getDate()
 }
-​
 function getMonthName() {
     return monthNames[date.getMonth()];
 }
-​
 getMonthName()
-​
 function getYear() {
     return date.getFullYear();
 }
-​
 //`Today is Sunday the 31 of January, 2021’
 function getDateFormat() {
     return `Today is ${getDayName()} the ${getDayOfMonth()} of ${getMonthName()}, ${getYear()}`
 }
-​
-console.log(getDayName(date));
+console.log(getDateFormat())
