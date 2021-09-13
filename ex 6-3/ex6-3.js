d14f8e142bea9c79a1bc04aaf98c329f5bfc9051
Shadi Rada <shakkyr@gmail.com>
2021-09-13 21:59:27 +0300

    6.3

---
+++ ex 6-3/ex6-3.js
@@ -0,0 +1,32 @@
+const arr = ["boo", "doooo", "hoo","ro"];
+let newArr = [];
+arr.forEach((item)=>{
+         
+        newArr.push(item.length);
+    }
+)
+
+console.log("the array elements length: ",newArr);
+
+
+
+
+
+
+
+
+
+
+
+//    newList.push(arr[0].length);
+//    newList.push(arr[1].length);
+//    newList.push(arr[2].length);
+//    newList.push(arr[3].length);
+// // function strinArr (arr) {
+// //     let newArr = [];
+// //     for (let i=0; i< arr.length  ; i++) 
+// //         newArr.push (arr[i].length);
+    
+// //       return newArr;  
+// // }
+// console.log(newList);
