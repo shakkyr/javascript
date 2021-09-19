09156a74dad0cc3d5a423e15bf70c2efef48c243
Shadi Rada <shakkyr@gmail.com>
2021-09-20 00:46:42 +0300

    11-1

---
+++ ex 11-1/ex11-1.js
@@ -0,0 +1,25 @@
+var library = [
+    {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true
+    },
+    {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true
+    },
+    {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false
+    }
+    ];
+
+
+    function read ( param) {
+        return param.filter((obj) => {
+            return obj.readingStatus })
+        
+    }
+console.log(read(library));
