3d835566382fdbfcb2b7b396d4a27be30d8949f2
Shadi Rada <shakkyr@gmail.com>
2021-09-20 00:15:05 +0300

    9-3

--- ex 9-3/ex9-3.js
+++ ex 9-3/ex9-3.js
@@ -1,15 +1,44 @@
-// const foods = ["falafel", "sabich", "hummus", "pizza with extra pineapple"];
-// foods.sort(); //// ascending to descending 
-// foods.reverse()//// Sort the array of strings from descending to ascending
-// console.log(foods);
-
-
-const foods = ["falafel", "Sabich", "hummus", "pizza with extra pineapple"];
-// let newFoods = [];
-// for (let i =0 ; i<foods.length ; i++){
-//     newFoods.push(foods[i].toUpperCase());
-// }
-
-
-foods.sort((a, b) => b-a);
+let foods = ["falafel", "sabich", "hummus", "pizza with extra pineapple"];
+foods.sort(); //// ascending to descending 
+foods.reverse()//// Sort the array of strings from descending to ascending
 console.log(foods);
+
+
+// ---------------------------     b ----------------------
+
+        //   ------------------ b1 --------------------
+let bigfoods = ["falafel", "Sabich", "hummus", "pizza with extra pineapple"];
+let newFoods = bigfoods.sort((a,b) => {
+    return a.toLowerCase()> b.toLowerCase() ? 1:a.toLowerCase()<b.toLowerCase() ? -1 : 0 })
+
+console.log(bigfoods);
+
+        //   --------------------- b2 ---------------------
+let newFoods2 = bigfoods.sort((a,b) => {
+return b.toLowerCase()> a.toLowerCase() ? 1:b.toLowerCase()<a.toLowerCase() ? -1 : 0 })
+
+console.log(bigfoods);
+
+
+
+
+// ------------------------------- c -------------------------------------
+
+const words = ["apple", "supercalifragilisticexpialidocious",
+"hi", "zoo"];
+
+function sort(arr) {
+    return arr.sort((x,y) => y.length - x.length);
+ }
+
+// let longWord = 0;
+// words.forEach(element => {
+//     if (element.length >= longWord)
+//        longWord = element.length
+
+//   return longWord;
+    
+// });
+
+
+console.log(sort(words));
