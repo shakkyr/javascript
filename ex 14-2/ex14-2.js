ddd78828ecd41cbaaa999067a1bc18b67f3ab039
Shadi Rada <shakkyr@gmail.com>
2021-09-21 22:07:59 +0300

    ex 14-2

---
+++ ex 14-2/ex14-2.js
@@ -0,0 +1,28 @@
+function findSmallest(a, b, c){
+    if (a > b && c> b){
+    return b;
+    } else if (a >c && b > c) {
+    return c;
+    } 
+    else return a
+    
+    }
+    console.log(findSmallest(66,2,56));
+
+
+
+    // 1. First find the line that contains the problem. Write it down.
+    // line 10
+    // and the code is not giving the smallest number
+
+
+    // 2. Which debug method did you use to find the bug?
+
+    // we have used the nodejs internal vs code
+
+
+    // // 3. Explain the bug in your own words.
+
+    // for the first sight we will think its a regular code with 
+    // typing mistake 
+    // but when we fix  and run the code we didnt get the required result we got 66 instead of 2
---
+++ ex 14-3/ex14-3.js
@@ -0,0 +1,4 @@
+function getSumOfEven(arr){
+    return arr[2] + arr[4] + arr[6] + arr[8] + arr[10];
+    }
+    getSumOfEven([1,2,3,4,5,6,7,8,9,10]);
