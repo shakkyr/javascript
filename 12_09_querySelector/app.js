542a47bd5477856e3bd88fe0128a480131e3ce39
Shadi Rada <shakkyr@gmail.com>
2021-09-26 13:14:45 +0300

    html.javascript

---
+++ 12_09_querySelector/app.js
@@ -0,0 +1,28 @@
+// ****************
+// querySelector
+// ****************
+//To find the first li on the page:
+document.querySelector('li');
+
+//To find the first element with class of special:
+document.querySelector('.special');
+
+//To find the first element with id of main (there should only be one...)
+document.querySelector('#main');
+
+// To find the first li with the class of special, nested inside of a ul, nested inside a section:
+document.querySelector('section ul li.special');
+
+const imagee= document.querySelector('bear-photo');
+const clicker = document.querySelector('input,')
+increace = (addEventListener())
+
+// ****************
+// querySelectorAll
+// ****************
+
+// To find ALL li's on the page:
+document.querySelectorAll('li');
+
+// To find ALL elements with the class of special on the page:
+document.querySelectorAll('.special');
---
+++ 17.2-text_area/index.js
@@ -0,0 +1,13 @@
+const textvalue = document.querySelector("#text"),
+      click = document.querySelector("#click"),
+      alert = document.querySelector("#demo")
+myFunction = () => {
+  
+  if (textvalue.value.length < 100) {
+   alert.innerHTML =
+      "YOU MUST WRITE MORE THAT 100 CHARACTERS";
+  } else if (x >= 100) {
+   alert.innerHTML = "";
+  }
+}
+click.addEventListener("click", myFunction);
