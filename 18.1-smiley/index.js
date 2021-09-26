fb364e4fe6f384c0984790c077fe4f8378769de0
Shadi Rada <shakkyr@gmail.com>
2021-09-27 00:14:30 +0300

    18-1

--- 18.1-smiley/index.js
+++ 18.1-smiley/index.js
@@ -2,24 +2,23 @@ const text = document.querySelector('.input'),
     button = document.querySelector('.submit'),
     error = document.querySelector('.error'),
     image = document.querySelector('.imgContainer');
-
+    
+    
 
 chekNumber =() => {
     image.innerHTML = '';
     error.innerHTML = '';
 
-    if(typeof(text.value)='number' ){        
-        error.innerHTML = ` dude enter just numbers`;
-    }
-    else {
-            for (let i = 0; i<text.value ; i++){
-        
-        
+    if(text.value == parseInt(text.value) && text.value < 100 ){  
+        for (let i = 0; i<text.value ; i++){
             image.innerHTML += '<img src="./smile.jpg" alt="">'
-    
+            image.style.size= '20px'
+        } 
         }
+
+    else error.innerHTML = ` dude enter just numbers`;   
+        error.style.color ='red'
     
-    }
 }   
 
 
