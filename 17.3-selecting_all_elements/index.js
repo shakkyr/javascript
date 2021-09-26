757f3f58ebaae4d5b648dcba476baef4f6eb1401
Shadi Rada <shakkyr@gmail.com>
2021-09-26 23:27:55 +0300

    17-3

--- 17.3-selecting_all_elements/index.js
+++ 17.3-selecting_all_elements/index.js
@@ -1,6 +1,6 @@
-const myFirstDiv = document.querySelector("#myFirstDiv");
-const mySecondDiv = document.querySelector("#mySecondDiv");
-const myThirdDiv = document.querySelector("#myThirdDiv");
-const myFourthDiv = document.querySelector("#myFourthDiv");
+// const myFirstDiv = document.querySelector("#myFirstDiv");
+// const mySecondDiv = document.querySelector("#mySecondDiv");
+// const myThirdDiv = document.querySelector("#myThirdDiv");
+// const myFourthDiv = document.querySelector("#myFourthDiv");
 
 const all =document.querySelectorAll('div');
