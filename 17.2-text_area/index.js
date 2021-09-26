f02c4630ffd8958562cf0cd838977c1339dcf48f
Shadi Rada <shakkyr@gmail.com>
2021-09-26 23:27:04 +0300

    17-2

--- 17.2-text_area/index.js
+++ 17.2-text_area/index.js
@@ -1,14 +1,16 @@
 const textvalue = document.querySelector(".text"),
       click = document.querySelector(".click"),
-      alert = document.querySelector(".warning")
+      alert = document.querySelector(".warning");
       console.log();
+      alert.style.color = 'brown';
 myFunction = () => {
   
   if (textvalue.value.length < 100) {
    alert.innerHTML =
-      "you shoud put 100 character";
+      "you shoud put 100 character ";
   } else if ( textvalue.value.length>= 100) {
-   alert.innerHTML = "";
+   alert.innerHTML = "now its ok";
+   alert.style.color = 'green';
   }
 }
 click.addEventListener("click", myFunction);
