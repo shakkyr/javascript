247d09494ceed8903f32aac3d7ef94238cf62c0f
Shadi Rada <shakkyr@gmail.com>
2021-09-27 20:29:38 +0300

    21-3

--- 21.3-forEachDOM/index.js
+++ 21.3-forEachDOM/index.js
@@ -1,14 +1,48 @@
-const orderList = document.createElement("ol");
-document.body.append(orderList)
+const users = [
+    {
+    id: 167464,
+    firstName: "Jimmy",
+    lastName: "Arnold",
+    email: "jimmya@gmail.com",
+    },
+    {
+    id: 578447,
+    firstName: "Martha",
+    lastName: "Goldman",
+    email: "gold@hotmail.com",
+    },
+    {
+    id: 864578,
+    firstName: "Tim",
+    lastName: "Burton",
+    email: "timmy.hotmail.com",
+    },
+    ];
+
+    // 1. Loop over the array with the forEach method and dynamically
+    // create an ordered list of the first and last names from the objects.
+
+
+let orderetList = document.createElement('ol');
+document.body.appendChild(orderetList);
+
 users.forEach(element => {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = element.firstName + ' ' + element.lastName
-    const attribute = document.createAttribute('data-id')
-    attribute.value = element.id
-    listItem.setAttributeNode(attribute)
-    console.log(listItem);
+    let listItem = document.createElement('li');
+    listItem.innerHTML = element.firstName +' '+ element.lastName;
+    orderetList.appendChild(listItem);
 
-    listItem.style.listStyle = "none"
-    orderList.append(listItem)
-
+    // 3. Create a custom data attribute called “data-id” and attach the id
+// value to each li.
+    listItem.setAttribute('data-id' , element.id);
+    
+    
 });
+
+
+// 2. Remove the bullet points of the ordered list with javaScript.
+orderetList.style.listStyle = 'none';
+
+
+
+
+
