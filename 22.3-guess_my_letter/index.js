f9a0d1dea63dc75e2940e64499d826c78c7c6a52
Shadi Rada <shakkyr@gmail.com>
2021-09-28 16:54:42 +0300

    22=3

--- 22.3-guess_my_letter/index.js
+++ 22.3-guess_my_letter/index.js
@@ -7,12 +7,14 @@ let headLine = document.createElement("h2"), // game name
 
 // ========================= linking ======================
 
-document.body.appendChild(headLine);
-document.body.appendChild(smallBox);
-document.body.appendChild(warning);
-document.body.appendChild(keyGuessed);
-document.body.appendChild(alreadyGuessed);
-document.body.appendChild(yesButton);
+document.body.append(
+  headLine,
+  smallBox,
+  warning,
+  keyGuessed,
+  alreadyGuessed,
+  yesButton
+);
 
 // ================= styling ==================================
 document.body.style.textAlign = "center";
@@ -52,34 +54,7 @@ yesButton.style.padding = "7px";
 yesButton.style.visibility = "hidden";
 
 // ======================= functions ========================
-const english = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+const english ="abcdefghijklmnopqrstuvwxyz";
 
 let guesses = [];
 function gettLetter() {
@@ -87,7 +62,6 @@ function gettLetter() {
   return english[random];
 }
 function checkLetter(event) {
-
   if (event.key.toLowerCase() == myLetter) {
     smallBox.innerHTML = event.key.toLowerCase();
     warning.style.color = "green";
@@ -99,31 +73,27 @@ function checkLetter(event) {
     keyGuessed.innerHTML = "Would you like to play again?";
     keyGuessed.style.fontSize = "25px";
     guesses = [];
-    document.removeEventListener("keydown",checkLetter);
-  }
-  
-  else if (english.includes(event.key.toLowerCase())) {
-      if( guesses.includes(event.key.toLowerCase())){
-        warning.innerHTML = "letter already clicked";
-      }
-    else {  
+    document.removeEventListener("keydown", checkLetter);
+  } else if (english.includes(event.key.toLowerCase())) {
+    if (guesses.includes(event.key.toLowerCase())) {
+      warning.innerHTML = "letter already clicked";
+    } else {
+      warning.style.color = "red";
+      warning.style.fontSize = "20px";
+      warning.style.fontWeight = "bold";
+      guesses.push(event.key.toLowerCase());
+      alreadyGuessed.innerHTML = guesses;
+      warning.innerHTML = "Nope, wrong letter";
+
+      console.log(guesses);
+    }
+  } else if (!english.includes(event.key.toLowerCase())) {
     warning.style.color = "red";
     warning.style.fontSize = "20px";
     warning.style.fontWeight = "bold";
-    guesses.push(event.key.toLowerCase());
     alreadyGuessed.innerHTML = guesses;
-    warning.innerHTML = "Nope, wrong letter";
-
-    console.log(guesses);}
+    warning.innerHTML = "please inter a valid letter";
   }
-
-    else if (!english.includes(event.key.toLowerCase())) {
-      warning.style.color = 'red';
-      warning.style.fontSize = '20px'
-      warning.style.fontWeight = 'bold'
-      alreadyGuessed.innerHTML = guesses;
-      warning.innerHTML = 'please inter a valid letter';
-     };
 }
 let myLetter = gettLetter();
 
@@ -132,5 +102,5 @@ let isFound = false;
 document.addEventListener("keydown", checkLetter);
 yesButton.addEventListener("click", () => {
   location.reload();
-//   document.addEventListener("keydown", checkLetter);
+  //   document.addEventListener("keydown", checkLetter);
 });
