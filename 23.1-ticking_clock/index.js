f1dd0dda6053fc34d1ef10a6989b714d7168190a
Shadi Rada <shakkyr@gmail.com>
2021-09-28 20:46:35 +0300

    23-1

--- 23.1-ticking_clock/index.js
+++ 23.1-ticking_clock/index.js
@@ -1,22 +1,50 @@
-const hours = document.querySelector('.hours')
-const minutes = document.querySelector('.minutes')
-const seconds = document.querySelector('.seconds')
-setInterval(() => {
-    let today = new Date();
-    let calchours = today.getHours();
-    let calcminutes = today.getMinutes();
-    let calcseconds = today.getSeconds();
+// ============================= digital clock =====================
 
-    if (calchours < 10)
-        hours.innerText = `0${calchours}`;
-    else
-        hours.innerText = `${calchours}`;
-    if (calcminutes < 10)
-        minutes.innerText = `0${calcminutes}`;
-    else
-        minutes.innerText = `${calcminutes}`;
-    if (calcseconds < 10)
-        seconds.innerText = `0${calcseconds}`;
-    else
-        seconds.innerText = `${calcseconds}`;
-}, 500);
+
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+setInterval(() => {
+  let today = new Date();
+  let realHours = today.getHours();
+  let realMinuts = today.getMinutes();
+  let realSeconds = today.getSeconds();
+
+  if (realHours < 10) {
+    hours.innerText = '0' +' ' + realHours;
+    }
+  else {hours.innerText = realHours;
+       }
+  if (realMinuts < 10) {
+    minutes.innerText = '0' +' ' + realMinuts;
+     }
+  else {minutes.innerText = realMinuts;
+}
+  if (realSeconds < 10) {
+    seconds.innerText = '0' +' ' + realSeconds;
+  }
+  else {seconds.innerText = realSeconds;}
+}, 1000);
+
+// ====================== analog clock =============================
+
+const secondsHand = document.querySelector(".second-hand");
+const minutesHand = document.querySelector(".min-hand");
+const hoursHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDeg = (seconds / 60) * 360 + 90;
+  secondsHand.style.transform = `translateY(-2px) rotate(${secondsDeg}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDeg = (mins / 60) * 360 + 90;
+  minutesHand.style.transform = `translateY(-3.5px) rotate(${minsDeg}deg)`;
+
+  const hours = now.getHours();
+  const hoursDeg = (hours / 12) * 360 + 90;
+  hoursHand.style.transform = `translateY(-5px) rotate(${hoursDeg}deg)`;
+}
+
+setInterval(setDate, 1000);
