4f76bae107778ca0ba1b4f52752950ef54564a9b
Shadi Rada <shakkyr@gmail.com>
2021-10-03 20:53:08 +0300

    27-1

--- 27.1-is_square_aTJjklj/index.js
+++ 27.1-is_square_aTJjklj/index.js
@@ -65,15 +65,14 @@
 
 
 function Square(a, b, c, d){
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    this.d = d;
-    this.isSquare = function () {
-            return this.a==this.b && this.a == this.c && this.a == this.d
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.isSquare = function () {
+                return this.a==this.b && this.a == this.c && this.a == this.d
         
-    }
-    }
-    let s = new Square(1,2,2,2);
-    console.log(s.isSquare());
-    
+        }
+        }
+        let s = new Square(1,2,2,2);
+        console.log(s.isSquare());
--- 27.2-pokemon/index.js
+++ 27.2-pokemon/index.js
@@ -1,39 +1,52 @@
-class Pokemon {
-  constructor(pokemonName, pokemonType, pokemonAttackList) {
-    this.name = pokemonName;
-    this.type = pokemonType;
-    this.attackList = pokemonAttackList;
-  }
-  callPokemon() {
-    console.log(`I choose you, ${this.name}`);
-  }
-  Attack(a) {
-    console.log(`${this.name} used ${this.attackList[a]} `);
-  }
-}
+// class Pokemon {
+//   constructor(pokemonName, pokemonType, pokemonAttackList) {
+//     this.name = pokemonName;
+//     this.type = pokemonType;
+//     this.attackList = pokemonAttackList;
+//   }
+//   callPokemon() {
+//     console.log(`I choose you, ${this.name}`);
+//   }
+//   Attack(a) {
+//     console.log(`${this.name} used ${this.attackList[a]} `);
+//   }
+// }
 
-let pikachu = new Pokemon("pikachu", "electric", [
-  "bite",
-  "thunderbolt",
-  "roll",
-]);
-let charlizard = new Pokemon("charlizard", "fire", [
-  "bite",
-  "fireball",
-  "superslam",
-]);
-let bolbasour = new Pokemon("bolbasour", "water", [
-  "bite",
-  "watercannon",
-  "groundsmash",
-]);
+// let pikachu = new Pokemon("pikachu", "electric", [
+//   "bite",
+//   "thunderbolt",
+//   "roll",
+// ]);
+// let charlizard = new Pokemon("charlizard", "fire", [
+//   "bite",
+//   "fireball",
+//   "superslam",
+// ]);
+// let bolbasour = new Pokemon("bolbasour", "water", [
+//   "bite",
+//   "watercannon",
+//   "groundsmash",
+// ]);
 
-pikachu.callPokemon();
-charlizard.callPokemon();
-pikachu.Attack(1);
-charlizard.Attack(1);
-pikachu.Attack(2);
-charlizard.Attack(2);
-bolbasour.callPokemon();
-bolbasour.Attack(1);
-bolbasour.Attack(2);
+// pikachu.callPokemon();
+// charlizard.callPokemon();
+// pikachu.Attack(1);
+// charlizard.Attack(1);
+// pikachu.Attack(2);
+// charlizard.Attack(2);
+// bolbasour.callPokemon();
+// bolbasour.Attack(1);
+// bolbasour.Attack(2);
+
+
+function Pokemon(pokemonName, pokemonType, pokemonAttackList){
+  this.name = pokemonName;
+  this.type = pokemonType;
+  this.attackList = pokemonAttackList;
+  }
+
+let pikachu = new Pokemon ('pikachu', 'electric', ['paralize', 'thunderBolt', 'quickAttack']);
+let squirtle = new Pokemon ('squirtle', 'water', ['waterGun', 'surf', 'quickAttack']); 
+let charmandr = new Pokemon ('charmandr', 'fire', ['dig', 'headAttack', 'roll']); 
+
+console.log(pikachu.type);
