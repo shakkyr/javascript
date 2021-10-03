5e244895b4f614dd44ea0933ee7701422eda38b9
Shadi Rada <shakkyr@gmail.com>
2021-10-03 21:30:10 +0300

    27-2

--- 27.2-pokemon/index.js
+++ 27.2-pokemon/index.js
@@ -1,44 +1,3 @@
-// class Pokemon {
-//   constructor(pokemonName, pokemonType, pokemonAttackList) {
-//     this.name = pokemonName;
-//     this.type = pokemonType;
-//     this.attackList = pokemonAttackList;
-//   }
-//   callPokemon() {
-//     console.log(`I choose you, ${this.name}`);
-//   }
-//   Attack(a) {
-//     console.log(`${this.name} used ${this.attackList[a]} `);
-//   }
-// }
-
-// let pikachu = new Pokemon("pikachu", "electric", [
-//   "bite",
-//   "thunderbolt",
-//   "roll",
-// ]);
-// let charlizard = new Pokemon("charlizard", "fire", [
-//   "bite",
-//   "fireball",
-//   "superslam",
-// ]);
-// let bolbasour = new Pokemon("bolbasour", "water", [
-//   "bite",
-//   "watercannon",
-//   "groundsmash",
-// ]);
-
-// pikachu.callPokemon();
-// charlizard.callPokemon();
-// pikachu.Attack(1);
-// charlizard.Attack(1);
-// pikachu.Attack(2);
-// charlizard.Attack(2);
-// bolbasour.callPokemon();
-// bolbasour.Attack(1);
-// bolbasour.Attack(2);
-
-
 function Pokemon(pokemonName, pokemonType, pokemonAttackList){
   this.name = pokemonName;
   this.type = pokemonType;
@@ -72,7 +31,8 @@ console.log('pikachu hp droped to 30%');
 pikachu.attack(0);
 console.log('charmandr paralizrd he cant move now');
 pikachu.attack(2);
-console.log('charmandr hp droped to 30%');
+console.log(`charmandr hp droped to 30%`);
+
 
 
 
