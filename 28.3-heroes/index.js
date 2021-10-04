ecf98a96ad183e0528cd50b63a2eb8d2d724282f
Shadi Rada <shakkyr@gmail.com>
2021-10-04 15:50:23 +0300

    28-3

---
+++ 28.3-heroes/index.js
@@ -0,0 +1,45 @@
+// Front end development
+// JavaScript – heroes
+// The following exercise contains the following subjects:
+// bind, call, apply
+// Instructions
+// We have an array of super heroes objects:
+// and a print name function:
+// We want to print the all heroes names. Implement the
+// printHeroes function: Note: you cannot change the super
+// heroes objects
+// const wonderWoman = {
+// name: "Diana Prince"
+// }
+// const batman = {
+// name: "Bruce Wayne"
+// }
+// const superHeroes = [wonderWoman, batman];
+// function printName() {
+// console.log(`my name is ${this.name}`);
+// }
+// function printHeroes(heroes, printFunc) {
+///add your code here
+// }
+
+
+const wonderWoman = {
+    name: "Diana Prince"
+    }
+    const batman = {
+    name: "Bruce Wayne"
+    }
+const superHeroes = [wonderWoman, batman];
+
+
+function printName() {
+        console.log(`my name is ${this.name}`);
+    }
+
+
+function printHeroes(heroes, printFunc) {
+            //add your code here
+            return heroes.map( e => printFunc.call(e))
+    }
+
+    printHeroes(superHeroes, printName);
